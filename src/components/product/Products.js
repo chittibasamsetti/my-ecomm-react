029e5d53d2e16159a4e1d54099acf62b30db6172
chittibasamsetti <chittibasamsetti@gmail.com>
2024-12-30 22:48:26 +0530

    my-ecomm-react

---
+++ src/components/product/Products.js
@@ -0,0 +1,33 @@
+import React from "react";
+import products from "./products.json";
+import "./Products.css";
+import AddButton from "./AddButton.js";
+export default function Products() {
+  return (
+    <div className="Product-container">
+      {products &&
+        products.map((value) => (
+          <div key={value.id} className="Product-items">
+            <div>
+              <img
+               
+                className="Product-img"
+                src={value.image}
+                alt={value.name}  
+              />
+            </div>
+            <h3>{value.name}</h3>
+            <p style={{ textAlign: "justify", padding: "10px" }}>
+              {value.desc}
+            </p>
+            <div className="priceBtn">
+              <div className="priceTxt">₹{value.price}</div>
+              <div className="Product-button">
+                <AddButton id={value.id} />
+              </div>
+               </div>
+          </div>
+        ))}
+    </div>
+  );
+}
